177b474facfa2faa5071ee3251a41aa9979bdff2
KaanKayserili <knkayserili@gmail.com>
2023-08-08 14:39:08 +0300

    general

--- components/SearchCalorie.js
+++ components/SearchCalorie.js
@@ -1,21 +1,62 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Button from './Button'
 import { Dimensions } from 'react-native'
+import { fetchFoodData } from '../service/api'
 
-const {width}=Dimensions.get("screen")
+const { width } = Dimensions.get("screen")
 
 const SearchCalorie = () => {
-    return (
-        <View>
-            <Text>Search and Add</Text>
+    const [food, setFood] = useState("");
 
-            <TextInput />
-            <Button text={"Add"} handleSubmit={() => { }} width={width * 0.5} marginLeft={width * 0.25} />
+    const searchFood = () => {
+        if (food !== "") {
+            fetchFoodData(food);
+        }
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Search and Add</Text>
+
+            <ScrollView>
+                <TextInput style={styles.input} value={food} onChangeText={(food) => setFood(food)} />
+                <Button text={"Add"} handleSubmit={searchFood} width={width * 0.5} marginLeft={width * 0.25} />
+                {
+
+                }
+            </ScrollView>
         </View>
     )
 }
 
 export default SearchCalorie
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "green",
+        flex: 1,
+        width: width,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "center",
+        paddingTop: 20,
+    },
+
+    input: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderWidth: 2,
+        borderColor: "gray",
+        borderRadius: 40,
+        backgroundColor: "white",
+        color: "green",
+        width: width * 0.7,
+        marginLeft: width * 0.15,
+        marginVertical: 20,
+    }
+})
